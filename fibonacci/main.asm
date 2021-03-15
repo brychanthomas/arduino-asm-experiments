edc1058cbe4c78d542e25674c7cca33ec8eaf070
Brychan Thomas <brychan.thomas@btinternet.com>
2021-03-15 09:14:24 +0000

    Added serial subroutines to fibonacci

--- fibonacci/main.asm
+++ fibonacci/main.asm
@@ -10,3 +10,27 @@
 start:
     inc r16
     rjmp start
+
+;initialise serial connection
+serial_init:
+	ldi r16, 103
+	clr r17
+
+	sts 0xc5, r17
+	sts 0xc4, r16
+
+	ldi	r16, (1<<4)|(1<<3)
+	sts	0xc1, r16
+
+	ldi	r16, 0b00001110
+	sts	0xc2, r16
+	ret
+
+; transmit byte stored in r19 over serial
+serial_transmit:
+	lds	r16, 0xc0
+	sbrs r16, 5
+	rjmp serial_transmit
+
+	sts 0xc6, r19
+	ret
